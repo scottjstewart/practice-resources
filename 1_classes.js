376ef5182245e95fa4c1b41a4dae6bea73d6cdb9
scottjstewart <scottstewart965@gmail.com>
2019-01-09 13:08:03 -0500

    updated classes file

--- 1_classes.js
+++ 1_classes.js
@@ -1,7 +1,7 @@
 class User {
-  constructor(name) {
+  constructor(name, type) {
     this.name = name;
-    this.type = "Trial User";
+    this.type = type;
   }
 
   greet() {
@@ -12,3 +12,46 @@ class User {
     console.log("Current status: " + this.type);
   }
 }
+
+class Details {
+  constructor(first, last, e) {
+    this.f = first;
+    this.l = last;
+    this.e = email;
+  }
+  greet() {
+    console.log("Weclome " + this.f);
+  }
+  details() {
+    console.log(
+      "Here are your profile details: " + "firstname: " + this.f,
+      "lastname: " + this.l,
+      "email: " + this.e
+    );
+  }
+}
+
+var userOne = new Details("Scott", "Stewart", "scottstewartj@gmail.com");
+userOne.greet();
+userOne.details();
+
+var anonDude = new User("Anonymous dude", "trial user");
+
+anonDude.greet();
+anonDude.status();
+
+var anonLady = new User("Anonymous Lady", "bronze user");
+anonLady.greet();
+anonLady.status();
+
+var jeff = new User("Jeff", "silver user");
+jeff.greet();
+jeff.status();
+
+var scott = new User("Scott Stewart", "Premium User");
+scott.greet();
+scott.status();
+
+var harry = new User("Harrison", "Gold User");
+harry.greet();
+harry.status();
